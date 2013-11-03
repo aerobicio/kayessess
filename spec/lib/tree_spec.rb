43661c3ec26476681c94c6801eb68bf7d07ff552
Justin Morris <justin@pixelbloom.com>
2013-11-03 16:56:55 +1100

    Spec Tree

--- spec/lib/tree_spec.rb
+++ spec/lib/tree_spec.rb
@@ -4,39 +4,39 @@ describe Kayessess::Tree do
   subject(:tree) { Kayessess::Tree.new(sections) }
   let(:section1) { double(Kayessess::Section) }
   let(:section2) { double(Kayessess::Section) }
+  let(:section3) { double(Kayessess::Section) }
   let(:sections) do
     [
       ["Buttons.Primary Button", section1],
-      ["Buttons.Secondary Button", section2]
+      ["Buttons.Secondary Button", section2],
+      ["Components.Headers", section3]
     ]
   end
 
-  describe "#initialize" do
-    before { tree.stub(:build_tree) }
-
-    it { should_receive(:build_tree) }
-  end
-
   describe "#root_sections" do
-    pending
+    subject { tree.root_sections }
+
+    it "returns the children of the styleguide root" do
+      expect(subject.length).to be 2
+      expect(subject[0].sections[0].id).to eq "primary-button"
+      expect(subject[0].sections[1].id).to eq "secondary-button"
+      expect(subject[1].sections[0].id).to eq "headers"
+    end
   end
 
   describe "#node_for_path" do
-    subject {
-      tree.node_for_path(section_path)
-    }
+    subject { tree.node_for_path(section_path) }
 
     context "when the path exists" do
-
       context "for a child node" do
         let(:section_path) { "Buttons".split('.') }
 
         it "returns the node that matches the path passed in" do
-          subject.id.should == :buttons
-          subject.parent.id.should == :root
-          subject.children.should == []
-          expect(subject.sections).to include :"primary button"
-          expect(subject.sections).to include :"secondary button"
+          expect(subject.id).to eq "buttons"
+          expect(subject.parent.id).to eq :root
+          expect(subject.children).to eq []
+          expect(subject.sections.map(&:id)).to include "primary-button"
+          expect(subject.sections.map(&:id)).to include "secondary-button"
         end
       end
 
@@ -44,7 +44,7 @@ describe Kayessess::Tree do
         let(:section_path) { "Buttons.Primary Button".split('.') }
 
         it "returns the node that matches the path passed in" do
-          subject.id.should == :"primary button"
+          expect(subject.id).to eq "primary-button"
         end
       end
     end
