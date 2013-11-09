1b91f155fb55ed70a7041c99d13b381c72c64a48
Justin Morris <justin@pixelbloom.com>
2013-11-09 21:48:43 +1100

    Ensure that host app helpers are available to kayessess. Also 0.1.4

--- lib/kayessess/engine.rb
+++ lib/kayessess/engine.rb
@@ -1,5 +1,15 @@
 module Kayessess
   class Engine < ::Rails::Engine
+
+    config.autoload_paths << File.expand_path("../lib", __FILE__)
     isolate_namespace Kayessess
+    engine_name "kayessess"
+
+    # Ensure that application helpers are available to kayessess.
+    # This is important as we need to make sure that application helpers can be
+    # used within styleguide examples.
+    config.to_prepare do
+      Kayessess::ApplicationController.helper Rails.application.helpers
+    end
   end
 end
