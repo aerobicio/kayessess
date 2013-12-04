061d614253d5d58ec9661cf8ed94c366495c59e1
Justin Morris <justin@pixelbloom.com>
2013-12-04 13:34:47 +1100

    Remove skip_before_filter

--- app/controllers/kayessess/application_controller.rb
+++ app/controllers/kayessess/application_controller.rb
@@ -2,7 +2,6 @@ module Kayessess
 
   # The ApplicationController is the class which all Kayessess rails controllers inherit from.
   class ApplicationController < ActionController::Base
-    skip_before_filter :login_required
     before_filter :parse_styles
 
   private
